a68ff5fe8d4411190afbd1099ef3a32dc9ae92df
Angel.Espinoza <angelo_19_94@hotmail.com>
2021-08-07 11:35:34 -0500

    Se termina app de votaciones

---
+++ lib/services/socket_service.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus{
+  Online,
+  Offline,
+  Connecting
+}
+
+class SocketService with ChangeNotifier{
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  late IO.Socket _socket;
+
+  ServerStatus get serverStatus => this._serverStatus;
+  IO.Socket get socket => this._socket;
+
+  SocketService(){
+    this._initConfig();
+  }
+
+  void _initConfig(){
+    this._socket = IO.io('http://192.168.1.72:3000',
+    IO.OptionBuilder()
+      .setTransports(['websocket'])
+      .enableAutoConnect()
+      .build());
+
+    _socket.onConnect((_) {
+      _serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });    
+
+    _socket.onDisconnect((_) {
+      _serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+
+    /* socket.on("nuevo-mensaje", (payload){
+      print("nuevo-mensaje");
+      print("nombre" + payload["nombre"]);
+      print("mensaje" + payload["mensaje"]);
+      print(payload.containsKey("mensaje2") ? payload["mensaje2"] : "No hay");
+    }); */
+
+
+  }
+
+}
